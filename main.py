271c028eefa20a65c62c715ac49343b3499b51ba
PooyaKhandel <pooya_khandel@omidtech.local>
2019-01-29 21:30:53 +0330

    important bug fixes

--- main.py
+++ main.py
@@ -4,6 +4,5 @@ from node import Node, CloudNet
 
 X_train, X_test, Y_train, Y_test = datasets.get_mvmc(te_percent=0.20)
 
-
 cl = CloudNet()
-cl.train_model(X_train[0], Y_train[0], 100, 5)
+cl.train_model(X_train['0'], Y_train['0'], 100, 5)
--- node.py
+++ node.py
@@ -8,10 +8,10 @@ from keras.utils import to_categorical
 class CnnModel:
     filter_num = 7
 
-    def __int__(self):
+    def __init__(self):
         self.model = keras.models.Sequential()
         self.optimizer = None
-        self.loss = 'binary_cross_entropy'
+        self.loss = 'binary_crossentropy'
         self.activation = 'relu'
         self.kernel_size = (3, 3)
         self.filter_num = CnnModel.filter_num
@@ -20,10 +20,11 @@ class CnnModel:
         self.dense_len = 4  # person, bus, car, not-present
 
     def conv2d_define(self):
-        return keras.layers.Conv2D(self.filter_num, self.kernel_size, strides=(1, 1), activation=self.activation)
+        return keras.layers.Conv2D(self.filter_num, self.kernel_size, strides=(1, 1), activation=self.activation,
+                                   padding='same')
 
     def max_pool_define(self):
-        return keras.layers.MaxPool2D(self.pool_size)
+        return keras.layers.MaxPooling2D(pool_size=self.pool_size, padding='same')
 
     def bath_normalization_define(self):
         return keras.layers.BatchNormalization()  # parameter config !
@@ -37,6 +38,7 @@ class CnnModel:
         return keras.layers.Dense(self.dense_len)
 
     def add_layers_fully(self):
+        self.model.add(keras.layers.Flatten())
         self.model.add(self.fully_define())
         self.model.add(self.bath_normalization_define())
 
@@ -49,17 +51,17 @@ class CnnModel:
         self.model.compile(loss=self.loss, optimizer=self.optimizer)
 
     def create_input(self, inp_shape):
-        self.model.add(keras.engine.input_layer.Input(shape=inp_shape))
+        self.model.add(keras.layers.InputLayer(input_shape=inp_shape))
 
     def train_model(self, X, Y, btch_size, ep):
-        self.model.fit(x=X, y=Y, batch_size=btch_size, epochs=ep)
+        self.model.fit(x=X, y=Y, epochs=ep)
 
 
 class Node:
     """"
     Node Class: each device is an object of this class
     """
-    def __int__(self, aidi):
+    def __init__(self, aidi):
         self.device_id = aidi
         self.inp_shape = (3, 32, 32)
         self.input = None
@@ -76,16 +78,16 @@ class Node:
 
 class CloudNet:
 
-    def __int__(self):
+    def __init__(self):
         self.device_id = -1
         self.input = None
         self.output = None
         self.model = CnnModel()
         self.train = 1
         if self.train == 1:
-            self.inp_shape = (3, 32, 32)
+            self.inp_shape = 3, 32, 32
             self.complexity = 3
-            self.create_input(self.inp_shape)
+            self.model.create_input(self.inp_shape)
             for _ in range(self.complexity):
                 self.model.add_layers_convp()
             self.model.add_layers_fully()
@@ -103,7 +105,9 @@ class CloudNet:
     def train_model(self, x, y, bt_s, eps):
         # if self.train == 1:
         y = to_categorical(y)
-        self.model.train_m(X=x, Y=y, btch_size=bt_s, ep=eps)
+        print(y.shape)
+        print(x.shape)
+        self.model.train_model(X=x, Y=y, btch_size=bt_s, ep=eps)
         # else:
         #     print("Not in training mode")
 
