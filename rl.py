5846cb07022f11b9621ed34ea42f0692489dd8e2
amir rassafi <amir.rassafi@gmail.com>
2019-01-31 10:38:35 +0330

    A design added for RL

--- rl.py
+++ rl.py
@@ -5,6 +5,19 @@ import keras
 
 
 class PolicyNetwork:
-    def __int__(self):
-
+    def __init__(self):
         pass
+
+class RL:
+    def __init__(self):
+        self.model = PolicyNetwork()
+    
+    def set_reward_plus_function(self):
+        pass
+
+    def set_reward_minus_function(self):
+        pass
+
+    def get_loss_function(self):
+        pass
+
