24a7714614bc35a6ee54818f0c52314ccee66682
amir rassafi <amir.rassafi@gmail.com>
2019-02-03 21:47:56 +0330

    focusing on developing rl.py, stil some bugs are not fixed, size of prediction for cloudnet doesn't match size of input prediction placeholder

--- rl.py
+++ rl.py
@@ -6,12 +6,13 @@ from node import CnnModel, CloudNet
 from scipy.stats import bernoulli
 import tensorflow as tf
 import numpy as np
+import time
 
 class PolicyNetwork:
     def __init__(self):
-        self.pnet = CnnModel(d_size=6)
+        self.pnet = CnnModel(d_size=4)
         self.alpha = 0.8
-        self.inp_shape = 16, 16, 42
+        self.inp_shape = 16, 16, 7
         self.input_tensor = self.pnet.add_inputs(inp_shape=self.inp_shape, num=6, name="pnet_input")
         convp_tensor = self.pnet.add_convp(inputs=self.input_tensor, parallel=-1, name="pnet_conv_1st")
         convp2_tensor = self.pnet.add_convp(inputs=convp_tensor, parallel=0, name="pnet_conv_2nd")
@@ -48,9 +49,13 @@ class RL:
     def bernoulli(self, s):
         pass
     
-    def train(self, input_data, epoch):
+    def train(self, input_data, y_label, epoch):
         pre = tf.placeholder(tf.float32, shape=(None, 1), name="predicted")#30 feature
         pnet_in, pnet_out = self.policy_network.get_in_out_tensor()
+
+        cl = CloudNet(0)
+        print(type(pre), type(pnet_in[0]))
+        # exit()
 
         u = pnet_out < tf.random_uniform(tf.shape(pnet_out))
         u = tf.cast(u, tf.float32)
@@ -73,11 +78,35 @@ class RL:
         reward = self.reward(tf.count_nonzero(u, axis=1, dtype=tf.float32), pre)
         temp = tf.multiply(temp, reward)
         loss = tf.reduce_mean(temp)
-        print(loss)
+        # print(loss)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
 
-        loss = tf.reduce_mean()
         ses = tf.InteractiveSession()
         ses.run(tf.global_variables_initializer())
+        f_dict = {}
+        for pi,id in zip(pnet_in, input_data):
+            f_dict[pi] = id
+        policy_output = ses.run(pnet_out, feed_dict=f_dict)
+        batch_size = 100
+        for e_itr in range(epoch):
+            train_loss = 0
+            # start = time.time()
+            # for indx  in range(input_data[0].shape[0]//batch_size):
+            prediction_res = cl.calculate(input_data, policy_output)
+            a = prediction_res.copy()
+            a[prediction_res == y_label] = 1
+            a[prediction_res != y_label] = 0
+            print(a.shape)
+            input_dict = {pre:a}
+            for pi,id in zip(pnet_in, input_data):
+                input_dict[pi] = id
+            _, policy_output = ses.run([optimizer, pnet_out], feed_dict=input_dict)
+            print(policy_output)
+            # train_loss += tl
+            # print(train_loss, "time = ", time.time()-start)
+        # print("full time is = ", time.time()-st)
+
+
         ses.close()
 
 
