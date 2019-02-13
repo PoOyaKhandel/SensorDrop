0c9f699a94dc4ec725f253c906f3614d88e89151
PooyaKhandel <pooya_khandel@omidtech.local>
2019-02-13 23:49:01 +0330

    some find to solve training bugs

--- cloudnet_test.py
+++ cloudnet_test.py
@@ -9,9 +9,12 @@ X_train, X_test, Y_train, Y_test = datasets.get_mvmc(te_percent=0.20)
 
 
 cl = CloudNet(train=1)
-cl.train_model(X_train, Y_train, bt_s=50, eps=20)
+cl.train_model(X_train, Y_train, bt_s=50, eps=2)
 
+print("cloud on")
 cl_test = CloudNet(train=0)
+# exit()
+print("node on")
 # instantiate node and cloud network
 nodes = []
 for i in range(6):
@@ -19,7 +22,8 @@ for i in range(6):
 
 node_outputs = []
 for n, i in zip(nodes, range(len(nodes))):
-    node_outputs.append(n.calculate(X_train[str(i)]))
+    node_outputs.append(n.calculate(X_test[str(i)]))
+
 
 cl_in, cl_out = cl_test.get_in_out_tensor()
 
@@ -28,12 +32,34 @@ for input, node_out in zip(cl_in, node_outputs):
     feed[input] = node_out
 
 
+# prediction_res = cl_test.model.pred(node_outputs)
+
+# with tf.Session() as ses:
+#     tf.global_variables_initializer().run()
+    # cl_test.model.load("cloud")
+    # cl_in, cl_out = cl_test.get_in_out_tensor()
+    # prediction_res = ses.run(cl_out, feed_dict=feed)
+
+# pred_res = []
+# for res in prediction_res:
+#     pred_res.append([np.argmax(res)])
+
+# print(acc(pred_res, Y_train))
+
+
 with tf.Session() as ses:
     tf.global_variables_initializer().run()
+    cl_test.model.load("cloud")
+    cl_in, cl_out = cl_test.get_in_out_tensor()
     prediction_res = ses.run(cl_out, feed_dict=feed)
 
 pred_res = []
 for res in prediction_res:
     pred_res.append([np.argmax(res)])
 
-print(acc(pred_res, Y_train))
+print(acc(pred_res, Y_test))
+
+
+
+print(cl.eval_model(X_train, Y_train))
+
