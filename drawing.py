6fd08baf184d28a09965b1912f561ccbb5e9a517
amir <amir.rassafi@gmail.com>
2019-04-06 20:55:29 +0430

    Drawing font added

--- drawing.py
+++ drawing.py
@@ -1,6 +1,10 @@
 import save_result as sr
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+
+plot_font = 14
+matplotlib.rcParams.update({'font.size': plot_font})
 
 Ks = [0.1, 0.2, 0.4, 0.8, 0.9]
 datas = sr.load_data(Ks)
@@ -22,10 +26,12 @@ fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
 for p in points:
-    ax.plot(p[1], p[2], 'p', markersize=10)
-    ax.annotate("K="+str(p[0]), (p[1], p[2] + 0.01))
+    ax.plot(p[1], p[2], 'p', markersize=plot_font+2)
+    ax.annotate("K="+str(p[0]), (p[1]-0.002, p[2] + 0.01))
 
 ax.grid()
-ax.set_xlabel("accuracy")
-ax.set_ylabel("overhead")
+ax.set_xlabel("accuracy", fontsize=plot_font)
+ax.set_ylabel("overhead", fontsize=plot_font)
+ax.tick_params(axis='both', which='major', labelsize=plot_font)
+
 plt.show()
